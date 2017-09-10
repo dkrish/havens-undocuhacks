1bc1b16c6305e7584fa04d2a0776ee0ee35e28b8
Dexter Krishnan <dexter.krishnan@dkla.net>
2017-09-10 08:21:44 -0700

    added google maps, updated content in menus etc.

--- js/init.js
+++ js/init.js
@@ -3,5 +3,11 @@
 
     $('.button-collapse').sideNav();
 
+    $(document).ready(function() {
+      $('select').material_select();
+    });
+
   }); // end of document ready
+
+
 })(jQuery); // end of jQuery name space
